e424c72d3fb2e579edcca01d0041c97b531d9059
huyuexin <103698371@qq.com>
2017-07-29 14:16:40 +0800

    hello python
    the first linux git add

---
+++ hello.py
@@ -0,0 +1,34 @@
+print('helloworld')
+l=[1,2,3,4,5]
+print (l)
+l[2]='hello'
+print(l)
+print(l[0])
+c=[10,11,12]
+l[2]=[1,2,'a','b',c]
+print(l);
+
+print (l[2]);
+print (l[2][3]);
+
+b=[1,2,3]
+c=b[1:3]
+c[0]=2
+print(b)
+print(c)
+l.append('hello')
+print(l)
+l.pop(1)
+print(l)
+l.pop(-1)
+print(l)
+print (l[1:2])
+name_new='Eric'
+he='hello'+' '+name_new
+print(he)
+print(he.upper())
+print(he.lower())
+name='ada lovelace'
+print (name.title())
+print (name.upper())
+print (name.lower())
